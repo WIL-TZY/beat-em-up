81935aa26a1650cd65020629ea25532df6dbfcae
WIL-TZY <willowitzy@gmail.com>
2024-10-28 03:20:10 -0300

    jump state

--- scripts/player/character.gd
+++ scripts/player/character.gd
@@ -1,26 +1,34 @@
 class_name Character extends CharacterBody3D
 
-enum StateMachine { IDLE, WALK }
+enum StateMachine { IDLE, WALK, JUMP }
 
 @export var speed := 2
 @export var jump_force := 5
 
 var state : StateMachine = StateMachine.IDLE
 var enter_state : bool = true
+var gravity : float = 9.8
 
 @onready var animated_sprite : AnimatedSprite3D = $AnimatedSprite
+@onready var shadow : Sprite3D = $Shadow
 
 # Get input (read-only)
 var input : Vector2: 
 	get: return Input.get_vector("move_left", "move_right", "move_up", "move_down") * speed
 
-func _physics_process(_delta: float) -> void:
+var jump: bool:
+	get: return Input.is_action_just_pressed("jump")
+
+func _physics_process(delta: float) -> void:
 	match state:
 		StateMachine.IDLE: __idle()
 		StateMachine.WALK: __walk()
+		StateMachine.JUMP: __jump()
 
+	__set_gravity(delta)
 	move_and_slide()
 
+### Functions
 func __enter_state(animation: String) -> void:
 	if enter_state:
 		enter_state = false
@@ -34,7 +42,9 @@ func __change_state(new_state: StateMachine) -> void:
 # Abstract methods (only updated in child)
 func __idle() -> void: pass
 func __walk() -> void: pass
+func __jump() -> void: pass
 
+### MOVE & IDLE
 func __movement() -> void:
 	velocity.x = input.x
 	velocity.z = input.y
@@ -46,3 +56,8 @@ func __stop_movement() -> void:
 func __flip() -> void:
 	if input.x:
 		animated_sprite.flip_h = true if input.x < 0 else false
+
+### JUMP
+func __set_gravity(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y -= gravity * delta
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -4,9 +4,30 @@ func __idle() -> void:
 	__enter_state("idle")
 	__stop_movement()
 	if input: __change_state(StateMachine.WALK)
+	if jump: __change_state(StateMachine.JUMP)
 
 func __walk() -> void:
 	__enter_state("walk")
 	__movement()
 	__flip()
 	if not input: __change_state(StateMachine.IDLE)
+	if jump: __change_state(StateMachine.JUMP)
+
+func __jump() -> void:
+	# Player can move while jumping
+	__movement()
+	# __flip()
+	
+	if enter_state:
+		enter_state = false
+		animated_sprite.play("jump")
+		velocity.y = jump_force
+		if shadow != null: shadow.hide()
+		
+		# Wait 0.2 seconds
+		await get_tree().create_timer(0.2).timeout 
+	
+	# Only runs this block after waiting for 0.2 seconds
+	if is_on_floor(): 
+		if shadow != null: shadow.show()
+		__change_state(StateMachine.IDLE)
