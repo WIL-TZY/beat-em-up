7ac2e85d827dd9c10aeed75b02669d023777fc27
WIL-TZY <willowitzy@gmail.com>
2024-10-30 23:33:17 -0300

    punk enemy ai (kinda buggy)

--- scripts/enemies/enemy.gd
+++ scripts/enemies/enemy.gd
@@ -8,19 +8,26 @@ enum EnemyState { IDLE, WALK, HURT, DOWN, UP, ATTACK, DIED }
 
 var state : EnemyState = EnemyState.IDLE
 var enter_state : bool = true
+var gravity : float = 9.8
+var death : bool
+var walk_timer : float
+var face_right : bool
+# var animation : String
 
 @onready var animated_sprite : AnimatedSprite3D = $AnimatedSprite
+@onready var timer_node: Timer = $Timer
+@onready var player: Player = %Player
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	match state:
 		EnemyState.IDLE: __idle()
-		EnemyState.WALK: __walk()
+		EnemyState.WALK: __walk(delta)
+		
+	__set_gravity(delta)
 
 ### Functions
-func __enter_state(animation: String) -> void:
-	if enter_state:
-		enter_state = false
-		animated_sprite.play(animation)
+func __set_animation(anim: String) -> void:
+	animated_sprite.play(anim)
 
 func __change_state(new_state: EnemyState) -> void:
 	if state != new_state:
@@ -29,4 +36,26 @@ func __change_state(new_state: EnemyState) -> void:
 
 # Abstract methods (only updated in children)
 func __idle() -> void: pass
-func __walk() -> void: pass
+func __walk(_delta: float) -> void: pass
+
+### MOVE & IDLE
+func __movement() -> void:
+	pass
+	
+func __stop_movement() -> void:
+	velocity.x = 0
+	velocity.z = 0
+
+func __flip() -> void:
+	# Check player direction
+	face_right = player.transform.origin.x > transform.origin.x
+	
+	# Flip sprite
+	animated_sprite.flip_h = face_right
+
+	# Flip attack collision
+
+### JUMP
+func __set_gravity(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y -= gravity * delta
--- scripts/enemies/punk.gd
+++ scripts/enemies/punk.gd
@@ -1,10 +1,70 @@
 extends Enemy
 
-func _ready() -> void:
-	state = EnemyState.IDLE
+var target_distance : Vector3 # Distance to player
 
 func __idle() -> void:
-	__enter_state("idle")
+	if enter_state:
+		enter_state = false
+		__set_animation("idle")
 
-func __walk() -> void:
-	__enter_state("walk")
+		# Wait a random amount of time in idle state
+		timer_node.wait_time = randf_range(1, 2)
+		timer_node.start()
+
+		# Automatically change to walk after timeout
+		await timer_node.timeout
+		__change_state(EnemyState.WALK)
+
+func __walk(delta) -> void:
+	if enter_state:
+		enter_state = false
+
+		# Wait a random amount of time in walk state
+		timer_node.wait_time = randf_range(2, 4)
+		timer_node.start()
+
+		# Automatically change to idle after timeout
+		await timer_node.timeout
+		__change_state(EnemyState.IDLE)
+
+	# =============================================
+	#  ============== AI MOVEMENT ===============
+	# This code executes before the timer timeout
+	# =============================================	
+	
+	# Distance to the player position
+	target_distance = player.transform.origin - self.transform.origin # Returns a Vector3
+	
+	# Horizontal Movement (depends on the player's horizontal direction)
+	self.velocity.x = target_distance.x / abs(target_distance.x)
+	
+	# Walks for 1s and 2s
+	walk_timer += delta
+	if walk_timer >= randf_range(1, 2): walk_timer = 0 # Reset timer
+	
+	# Vertical Movement
+	if self.transform.origin.z < player.transform.origin.z:
+		self.velocity.z = randi_range(0, 1) 
+	else: 
+		self.velocity.z = randi_range(-1, 0)
+
+	# Stops 1 meter before reaching the player position
+	if (abs(target_distance.x) < 1):
+		self.velocity.x = 0
+
+	# __set_animation("idle" if not self.velocity else "walk")
+	if not self.velocity:
+		__set_animation("idle")
+	else:
+		__set_animation("walk")
+
+	__flip() # Only flips towards the player while in walk state
+	move_and_slide()
+
+# (DEBUG) Draw state
+func _process(_delta: float) -> void:
+	var label1 : Label = $"Debug Label1"
+	var label2 : Label = $"Debug Label2"
+	var text = str(EnemyState.keys()[state])
+	label1.text = text
+	label2.text = str(target_distance)
